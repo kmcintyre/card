5c1bf09a1d4b5f0276ffc2392e15e271d44fd258
Kevin McIntyre <kmcintyre@greenchargenet.com>
2013-09-25 23:57:13 -0700

    Another day - no dollar; learning Service and IntentService

--- html/node/ws.js
+++ html/node/ws.js
@@ -1,17 +1,32 @@
-var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 8080});
+var WebSocketServer = require('ws').Server,
+	cards = require('../card_deck'), 
+	wss = new WebSocketServer({port: 8080});
 
 console.log('started');
 
 wss.on('connection', function(ws) {
+	
 	console.log('connection');
+    
     ws.on('message', function(message) {
         console.log('received: %s', message);
         wss.broadcast(message);
     });
-    ws.send('something');
+    
+    ws.on('close', function() {
+        console.log('close');
+		wss.broadcast('disconnect');
+    });
+            
 });
 
-wss.broadcast = function(data) {
-    for(var i in this.clients)
-        this.clients[i].send(data);
+wss.broadcast = function(data, ws) {	
+	for(var i in this.clients) {
+		try {
+			console.log('broadcast client:' + this.clients[i]);
+    		this.clients[i].send(data);
+	    } catch (err) {
+	    	console.err('error:' + err);
+	    }        		
+	}    	
 };
